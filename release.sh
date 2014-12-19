072cd4053b709ce3f5b6cee91a7c5836cc526925
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-12-19 22:14:20 +0100

    - [X] Add release script

---
+++ release.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+if [ $# -ne 1 ]; then
+    cat <<EOF
+Use: $0 <VERSION>"
+- VERSION    version to release (0.1.6 for example)
+
+To install the token, execute the install-marmalade-token.sh.
+EOF
+    exit 1;
+fi
+
+WDIR=$(dirname $0)
+
+# launched from the current dev branch
+
+git fetch -p --all
+
+git checkout master
+
+git merge origin/master
+
+git tag $VERSION
+
+git push origin --tag
+
+make package
