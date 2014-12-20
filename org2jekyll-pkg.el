57f3e23f2dc99710014453afadb27184ca523b73
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-12-20 10:15:08 +0100

    - [X] Update version

--- org2jekyll-pkg.el
+++ org2jekyll-pkg.el
@@ -1,4 +1,4 @@
-(define-package "org2jekyll" "0.0.5" "Minor mode to publish org-mode post to jekyll without specific yaml"
+(define-package "org2jekyll" "0.0.6" "Minor mode to publish org-mode post to jekyll without specific yaml"
   '((deferred "0.3.2")
     (s "1.9.0")
     (dash "2.8.0")))
