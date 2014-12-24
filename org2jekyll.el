58d52dbbf99809caf54fdbb54e42daf51fe04fe2
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-12-24 13:45:56 +0100

    - [X] Improve doc (header + README.org + customization group + install step)

--- org2jekyll.el
+++ org2jekyll.el
@@ -31,12 +31,14 @@
 ;; Functions to ease publishing jekyll posts from org-mode file
 
 ;; Providing you have a working `'jekyll`' and `'org-publish`'
-;; This will permit you to simply export an org-mode file to the right format to
-;; the right folder for jekyll to understand
+;; This will permit you to simply export an org-mode file with the right jekyll
+;; format to the right folder
 ;;
 ;; M-x org2jekyll/create-draft! create a draft with the necessary metadata
 ;;
-;; M-x org2jekyll/publish-post! publish the post to the jekyll folder
+;; M-x org2jekyll/publish! publish the post (or page) to the jekyll folder
+;;
+;; You can customize using M-x customize-group RET org2jekyll RET
 ;;
 ;; More information on https://github.com/ardumont/org2jekyll
 
