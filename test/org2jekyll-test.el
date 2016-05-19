b4d5ba00df61669c639d6d53d2798829c3059908
Scott McLeod <halcyonblue@gmail.com>
2016-05-19 11:56:48 -0400

    Add test for org2jekyll-get-options-from-buffer

--- test/org2jekyll-test.el
+++ test/org2jekyll-test.el
@@ -5,6 +5,18 @@
 (require 'el-mock)
 
 ;;; Code:
+(ert-deftest test-org2jekyll-get-options-from-buffer ()
+  (let* ((blog-key "#+BLOG:")
+         (blog-val "tony's blog")
+         (date-key "#+DATE:")
+         (date-val "some-date")
+         (options-plist (with-temp-buffer (insert (concat blog-key " " blog-val "\n"
+                                                          date-key " " date-val "\n"
+                                                          "Beef fungus articles"))
+                                          (org2jekyll-get-options-from-buffer))))
+    (should (string= blog-val (plist-get options-plist :blog)))
+    (should (string= date-val (plist-get options-plist :date)))))
+
 (ert-deftest test-org2jekyll-get-options-from-file ()
   (let* ((temp-file "/tmp/test-get-options-from-file")
          (blog-key "#+BLOG:")
